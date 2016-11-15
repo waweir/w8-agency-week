83dbbe0accc7eb67a322bf060c4ead935d4bf006
Jeff Bumgardner <bumgardner.jeffrey@gmail.com>
2016-11-15 12:51:17 -0500

    header in Main. minor css tweaks (logo switch from id to class)

--- components/Main.js
+++ components/Main.js
@@ -7,20 +7,28 @@ class Main extends React.Component {
     }
     render() {
         return <div>
-        <div className="panel panel-default">
-            <div className="panel-body">
-            <h4>This will be the header</h4>
-            {/* Header with links to socks and cart. The socks link can maybe be the logo and the cart link can be the image of a cart */}
-            <div className="col-sm-4">
-            <Link to="/socks">Test link to socks</Link>
-            </div>
-            <div className="col-sm-4">
-                Search bar
-            </div>
-            <div className="col-sm-4">
-                <Link to="/cart">Test link to cart</Link>
-            </div>
-            </div>
+            <div className="panel panel-default">
+                <div className="panel-body">
+                {/* Header with links to socks and cart. The socks link can maybe be the logo and the cart link can be the image of a cart */}
+                <div className="col-sm-4">
+                <Link to="/socks"><img alt="Brand" className="logo" src="img/sock-o-rama.png" /></Link>
+                </div>
+                <div className="col-sm-4">
+                    <form className="navbar-form navbar-left" role="search">
+                      <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Search" />
+                      <button type="submit" className="btn btn-default">Submit</button>
+                      </div>
+                    </form>
+                </div>
+                <div className="col-sm-3 col-sm-offset-1">
+                    <Link to="/cart">
+                    {/* TODO: where # is, put the counter of items.*/}
+                        <div className="glyphicon glyphicon-shopping-cart"> #
+                        </div>
+                    </Link>
+                </div>
+                </div>
             </div>
             <div>
                 <p>This will display the child pages</p>
