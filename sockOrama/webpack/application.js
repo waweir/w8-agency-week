7e445c09105bc202e6a15e60c858e6b6810298de
ccflack <ccflack@me.com>
2016-11-15 17:23:51 -0500

    Fixed script issue with event listener

--- sockOrama/webpack/application.js
+++ sockOrama/webpack/application.js
@@ -2,17 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
-import Main from '../webpack/Main'
-import SockDisplay from '../webpack/SockDisplay'
-import CartDisplay from '../webpack/CartDisplay'
+import Main from './Main'
+import SockDisplay from './SockDisplay'
+import CartDisplay from './CartDisplay'
 
-ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={SockDisplay} />
-            <Route path="/socks" component={SockDisplay} />
-            <Route path="/cart" component={CartDisplay} />
-        </Route>
-    </Router>
-    , document.getElementById('socks')
-)
+document.addEventListener('DOMContentLoaded', function() {
+
+  ReactDOM.render(
+      <Router history={browserHistory}>
+          <Route path="/" component={Main}>
+              <IndexRoute component={SockDisplay} />
+              <Route path="/socks" component={SockDisplay} />
+              <Route path="/cart" component={CartDisplay} />
+          </Route>
+      </Router>
+      , document.getElementById('socks')
+  )
+
+})
--- sockOrama/webpack/webpack.config.js
+++ sockOrama/webpack/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   entry: {
-    global: './js/global.js',
+    global: './application.js',
   },
   output: {
     path: './js/',
