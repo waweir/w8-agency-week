31dab0c23aebead6edd810773208d56dbc6a3a1b
Whitney Weir <whitneyweir36@gmail.com>
2016-11-15 12:17:12 -0500

    Updating React Router with IndexRoute

--- js/global.js
+++ js/global.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
 import Main from '../components/Main'
 import SockDisplay from '../components/SockDisplay'
@@ -9,6 +9,7 @@ import CartDisplay from '../components/CartDisplay'
 ReactDOM.render(
     <Router history={browserHistory}>
         <Route path="/" component={Main}>
+            <IndexRoute component={SockDisplay}/>
             <Route path="/socks" component={SockDisplay} />
             <Route path="/cart" component={CartDisplay} />
         </Route>
